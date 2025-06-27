8a59ec11c485af898251f094ffbccda0b5fc53dc
dikcoding <andikafadillasiagian0@gmail.com>
2025-06-27 14:06:28 +0700

    first commit

---
+++ models/src/src_riviews.sql
@@ -0,0 +1,14 @@
+WITH raw_reviews AS (
+    SELECT
+        *
+    FROM
+        {{ source('airbnb', 'riviews') }}
+)
+SELECT
+    listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments AS review_text,
+    sentiment AS review_sentiment
+FROM
+    raw_reviews
